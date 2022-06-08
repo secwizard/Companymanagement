3c608370aea66a2f1ac5b60703f4422d1357d330
developer <developer@wizardcomm.net>
2022-06-08 11:46:50 +0530

    Chsanges

--- CompanyManagement.Api/Service/TemplateService.cs
+++ CompanyManagement.Api/Service/TemplateService.cs
@@ -894,43 +894,42 @@ namespace CompanyManagement.Api.Service
             return retVal;
         }
 
-        //public async Task<TemplateSectionForMetaData> SaveUpdateCompanyTemplateSectionData(ResponseCompanyTemplate request)
-        //{
-        //    try
-        //    {
-        //        var parms = new SqlParameter[]
-        //        {
-        //            new SqlParameter("@FaqId", request.FaqId),
-        //            new SqlParameter("@CompanyId", request.CompanyId),
-        //            new SqlParameter("@Faq", request.Faq??""),
-        //            new SqlParameter("@FaqAnswer", request.FaqAnswer),
-        //            new SqlParameter("@FaqPostedBy", request.FaqPostedBy == null ? new Guid() : request.FaqPostedBy),
-        //            new SqlParameter("@FaqUpdatedBy", request.FaqUpdatedBy == null ? new Guid() : request.FaqUpdatedBy),
-        //            new SqlParameter("@AnswerPostedBy", request.AnswerPostedBy == null ? new Guid() : request.AnswerPostedBy),
-        //            new SqlParameter("@AnswerUpdatedBy", request.AnswerUpdatedBy == null ? new Guid() : request.AnswerUpdatedBy),
-        //            new SqlParameter("@ItemId", request.ItemId),
-        //            new SqlParameter("@IsActive",true),
-        //            new SqlParameter("@FaqUpdatedByName", request.FaqUpdatedByName??""),
-        //            new SqlParameter("@FaqPostedByName", request.FaqPostedByName??""),
-        //            new SqlParameter("@AnswerPostedByName",  request.AnswerPostedByName??""),
-        //            new SqlParameter("@AnswerUpdatedByName", request.AnswerUpdatedByName??""),
-        //            new SqlParameter("@ItemVariant", request.ItemVariant )
+        public async Task<TemplateSectionForMetaData> SaveUpdateCompanyTemplateSectionData(ResponseCompanyTemplate request)
+        {
+            //try
+            //{
+            //    var parms = new SqlParameter[]
+            //    {
+            //        new SqlParameter("@FaqId", request.FaqId),
+            //        new SqlParameter("@CompanyId", request.CompanyId),
+            //        new SqlParameter("@Faq", request.Faq??""),
+            //        new SqlParameter("@FaqAnswer", request.FaqAnswer),
+            //        new SqlParameter("@FaqPostedBy", request.FaqPostedBy == null ? new Guid() : request.FaqPostedBy),
+            //        new SqlParameter("@FaqUpdatedBy", request.FaqUpdatedBy == null ? new Guid() : request.FaqUpdatedBy),
+            //        new SqlParameter("@AnswerPostedBy", request.AnswerPostedBy == null ? new Guid() : request.AnswerPostedBy),
+            //        new SqlParameter("@AnswerUpdatedBy", request.AnswerUpdatedBy == null ? new Guid() : request.AnswerUpdatedBy),
+            //        new SqlParameter("@ItemId", request.ItemId),
+            //        new SqlParameter("@IsActive",true),
+            //        new SqlParameter("@FaqUpdatedByName", request.FaqUpdatedByName??""),
+            //        new SqlParameter("@FaqPostedByName", request.FaqPostedByName??""),
+            //        new SqlParameter("@AnswerPostedByName",  request.AnswerPostedByName??""),
+            //        new SqlParameter("@AnswerUpdatedByName", request.AnswerUpdatedByName??""),
+            //        new SqlParameter("@ItemVariant", request.ItemVariant )
 
-        //        };
-        //        string sqlText = $"EXECUTE dbo.SP_SaveUpdateFAQ  @FaqId, @CompanyId, @Faq, @FaqAnswer, @FaqPostedBy, @FaqUpdatedBy, @AnswerPostedBy, @AnswerUpdatedBy, @ItemId, @IsActive, @FaqUpdatedByName, @FaqPostedByName, @AnswerPostedByName, @AnswerUpdatedByName,@ItemVariant";
+            //    };
+            //    string sqlText = $"EXECUTE dbo.SP_SaveUpdateFAQ  @FaqId, @CompanyId, @Faq, @FaqAnswer, @FaqPostedBy, @FaqUpdatedBy, @AnswerPostedBy, @AnswerUpdatedBy, @ItemId, @IsActive, @FaqUpdatedByName, @FaqPostedByName, @AnswerPostedByName, @AnswerUpdatedByName,@ItemVariant";
 
-        //        var retval = await _context.FAQModel.FromSqlRaw(sqlText, parms).ToListAsync();
+            //    var retval = await _context.FAQModel.FromSqlRaw(sqlText, parms).ToListAsync();
 
-        //        return retval.FirstOrDefault();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        log.Info($"ErrorOn:{DateTime.UtcNow} Message:{ex.Message} InnerException: {ex.InnerException} StackTrace: {ex.StackTrace}");
-        //        throw;
-        //    }
-        //    // return new FAQModel();
-        //}
-
-
+            //    return retval.FirstOrDefault();
+            //}
+            //catch (Exception ex)
+            //{
+            //    log.Info($"ErrorOn:{DateTime.UtcNow} Message:{ex.Message} InnerException: {ex.InnerException} StackTrace: {ex.StackTrace}");
+            //    throw;
+            //}
+            //// return new FAQModel();
+            return new TemplateSectionForMetaData();
+        }
     }
 }
